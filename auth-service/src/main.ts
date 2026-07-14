9dcf22da4e7149082748f2fc2c4ace2d46aed853
Ayoub Badia <ayoub.badia95@gmail.com>
2026-07-14 01:02:04 +0100

    Fix git conflicts + Add auth_service in migration

--- auth-service/src/main.ts
+++ auth-service/src/main.ts
@@ -1,20 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-// @ts-ignore
-import { Transport, MicroserviceOptions } from '@nestjs/microservices';
 
 async function bootstrap() {
-<<<<<<< Updated upstream
-    const app =
-        await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
-            transport: Transport.TCP,
-            options: {
-                host: process.env.HOST || '0.0.0.0',
-                port: Number(process.env.PORT) || 3001,
-            },
-        });
-=======
   const app = await NestFactory.create(AppModule);
   
   // Enable CORS
@@ -29,19 +17,4 @@ async function bootstrap() {
   );
   await app.listen(process.env.PORT ?? 3000);
 }
-bootstrap();
->>>>>>> Stashed changes
-
-    app.useGlobalPipes(
-        new ValidationPipe({
-            whitelist: true,
-            forbidNonWhitelisted: true,
-            transform: true,
-        }),
-    );
-
-    await app.listen();
-    console.log('✅ Auth Service TCP running on port 3001');
-}
-
 bootstrap();
